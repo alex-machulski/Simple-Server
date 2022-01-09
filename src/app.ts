f3c85871409bb79985578c03c9a9edbd322bbcbd
DESKTOP-V8LFEN4\user <aliaksandr.machulski@gmail.com>
2022-01-09 15:35:37 +0300

    used inversify

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
 import express, {Express} from 'express';
 import {Server} from 'http';
 import {ExceptionFilter} from './errors/exception.filter';
-import {LoggerService} from './logger/logger.service';
 import {UserController} from './users/users.controller';
 import {ILogger} from "./logger/logger.interface";
+import {inject, injectable} from "inversify";
+import {TYPES} from "./types";
+import "reflect-metadata";
 
+@injectable()
 export class App {
     app: Express;
     server: Server;
     port: number;
-    logger: LoggerService;
-    userController: UserController;
-    exceptionFilter: ExceptionFilter;
 
-    constructor(logger: ILogger, userController: UserController, exceptionFilter: ExceptionFilter) {
+    constructor(
+        @inject(TYPES.ILogger) private loggerService: ILogger,
+        @inject(TYPES.UserController) private userController: UserController,
+        @inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter
+    ) {
         this.app = express();
         this.port = 8000;
-        this.logger = logger;
-        this.userController = userController;
-        this.exceptionFilter = exceptionFilter;
     }
 
     useRoutes() {
@@ -33,6 +34,6 @@ export class App {
         this.useRoutes();
         this.useExceptionFilters();
         this.server = this.app.listen(this.port);
-        this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+        this.loggerService.log(`Сервер запущен на http://localhost:${this.port}`);
     }
 }
