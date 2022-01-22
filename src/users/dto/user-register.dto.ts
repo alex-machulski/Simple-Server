03bcf855597651192b5c2ef2c0e1e66d30ce5b47
DESKTOP-V8LFEN4\user <aliaksandr.machulski@gmail.com>
2022-01-22 20:58:32 +0300

    added validation for user register

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import {IsEmail, IsString} from "class-validator";
+
 export class UserRegisterDto {
+    @IsEmail({}, {message: 'Wrong email'})
     email: string;
+
+    @IsString({message: 'Password was not entered'})
     password: string;
+
+    @IsString({message: 'Name was not entered'})
     name: string;
 }
