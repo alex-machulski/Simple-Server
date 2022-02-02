cabb4e21ebf5ef428ee8646f96d5242b49ec9e50
DESKTOP-V8LFEN4\user <aliaksandr.machulski@gmail.com>
2022-02-02 21:25:42 +0300

    added users repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -39,6 +39,6 @@ export class UserController extends BaseController implements IUserController {
         if (!result) {
             return next(new HTTPError(422, 'This user already exists'));
         }
-        this.ok(res, {email: result.email});
+        this.ok(res, {email: result.email, id: result.id});
     }
 }
