b48dbcd2330cacfcc24c3642c8b11a99d499695e
DESKTOP-V8LFEN4\user <aliaksandr.machulski@gmail.com>
2022-02-02 19:24:38 +0300

    added config service

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,47 @@
-import express, { Express } from 'express';
-import { Server } from 'http';
-import { ExceptionFilter } from './errors/exception.filter';
-import { UserController } from './users/users.controller';
-import { ILogger } from './logger/logger.interface';
-import { inject, injectable } from 'inversify';
-import { TYPES } from './types';
-import { json } from 'body-parser';
+import express, {Express} from 'express';
+import {Server} from 'http';
+import {ILogger} from './logger/logger.interface';
+import {inject, injectable} from 'inversify';
+import {TYPES} from './types';
+import {json} from 'body-parser';
 import 'reflect-metadata';
+import {IConfigService} from "./config/config.service.interface";
+import {IExceptionFilter} from "./errors/exception.filter.interface";
+import {UserController} from "./users/users.controller";
 
 @injectable()
 export class App {
-	app: Express;
-	server: Server;
-	port: number;
+    app: Express;
+    server: Server;
+    port: number;
 
-	constructor(
-		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserController) private userController: UserController,
-		@inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
-	) {
-		this.app = express();
-		this.port = 8000;
-	}
+    constructor(
+        @inject(TYPES.ILogger) private loggerService: ILogger,
+        @inject(TYPES.UserController) private userController: UserController,
+        @inject(TYPES.ExceptionFilter) private exceptionFilter: IExceptionFilter,
+        @inject(TYPES.ConfigService) private configService: IConfigService,
+    ) {
+        this.app = express();
+        this.port = 8000;
+    }
 
-	useMiddleware(): void {
+    useMiddleware(): void {
         this.app.use(json());
-	}
+    }
 
-	useRoutes(): void {
-		this.app.use('/users', this.userController.router);
-	}
+    useRoutes(): void {
+        this.app.use('/users', this.userController.router);
+    }
 
-	useExceptionFilters(): void {
-		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
-	}
+    useExceptionFilters(): void {
+        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+    }
 
-	public async init(): Promise<void> {
-		this.useMiddleware();
-		this.useRoutes();
-		this.useExceptionFilters();
-		this.server = this.app.listen(this.port);
-		this.loggerService.log(`Сервер запущен на http://localhost:${this.port}`);
-	}
+    public async init(): Promise<void> {
+        this.useMiddleware();
+        this.useRoutes();
+        this.useExceptionFilters();
+        this.server = this.app.listen(this.port);
+        this.loggerService.log(`Сервер запущен на http://localhost:${this.port}`);
+    }
 }
